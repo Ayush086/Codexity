a065a2a91e06112b3b0ea92796caee053f370018
Ayush Uttarwar <111568550+Ayush086@users.noreply.github.com>
2024-06-13 14:33:53 +0530

    Create CourseProgress.js

---
+++ server/models/CourseProgress.js
@@ -0,0 +1,18 @@
+// course progress schema
+
+const mongoose = require('mongoose');
+
+const courseProgressSchema = new mongoose.Schema({
+    courseId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Course",
+    },
+    completedVideos: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "SubSection",
+        }
+    ],
+});
+
+module.exports = mongoose.model("courseProgress", courseProgressSchema);
