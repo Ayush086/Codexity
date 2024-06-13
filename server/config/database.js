d008485c5f825c99681b9b940f85d9f0158d550c
Ayush Uttarwar <111568550+Ayush086@users.noreply.github.com>
2024-06-13 14:28:02 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,1 +1,16 @@
+// mongodb connection
 
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+exports.dbConnection = () => {
+    mongoose.connect()
+    .then( () => {
+        console.log("db connection successful");
+    })
+    .catch((error) => {
+        console.log("db connection failed");
+        console.error(error);
+        process.exit(1);
+    })
+}
