7345c879a30bf272d6319af85e9c5686b764c5d4
Ayush Uttarwar <111568550+Ayush086@users.noreply.github.com>
2025-01-20 23:53:36 +0530

    Create footer-links.js

---
+++ src/data/footer-links.js
@@ -0,0 +1,52 @@
+export const FooterLink2 = [
+    {
+        title: "Subjects",
+        links: [
+            { title: "AI", link: "/ai" },
+            { title: "Cloud Computing", link: "/cloud-computing" },
+            { title: "Code Foundations", link: "/code-foundations" },
+            { title: "Computer Science", link: "/computer-science" },
+            { title: "Cyber Security", link: "/cybersecurity" },
+            { title: "Data Analytics", link: "/data-analytics" },
+            { title: "Data Science", link: "/data-science" },
+            { title: "DevOps", link: "/devops" },
+            { title: "Game Development", link: "/game-development" },
+            { title: "IT", link: "/it" },
+            { title: "Machine Learning", link: "/machine-learning" },
+            { title: "Mobile Development", link: "/mobile-development" },
+            { title: "Web Design", link: "/web-design" },
+            { title: "Web Development", link: "/web-development" },
+        ],
+    },
+    {
+        title: "Languages",
+        links: [
+            { title: "C++", link: "/cpp" },
+            { title: "JavaScript", link: "/javascript" },
+            { title: "Python", link: "/python" },
+            { title: "Java", link: "/java" },
+            { title: "C#", link: "/csharp" },
+            { title: "PHP", link: "/php" },
+            { title: "TypeScript", link: "/typescript" },
+            { title: "Swift", link: "/swift" },
+            { title: "Go", link: "/go" },
+            { title: "Ruby", link: "/ruby" },
+            { title: "Rust", link: "/rust" },
+            { title: "Kotlin", link: "/kotlin" },
+            { title: "R", link: "/r" },
+            { title: "MATLAB", link: "/matlab" },
+            { title: "Bash", link: "/bash" }
+        ]
+    },
+    {
+        title: "Career building",
+        links: [
+            { title: "Career paths", link: "/career-paths" },
+            { title: "Career services", link: "/career-services" },
+            { title: "Interview prep", link: "/interview-prep" },
+            { title: "Professional certification", link: "/prefessional-certification" },
+            { title: "Full Catalog", link: "/full-catalog" },
+            { title: "Beta Content", link: "/beta-content" }
+        ]
+    }
+];
