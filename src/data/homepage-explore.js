075d94142d436b3fb0dd737e045c2ab1655a3d08
Ayush Uttarwar <111568550+Ayush086@users.noreply.github.com>
2025-01-20 23:54:05 +0530

    Create homepage-explore.js

---
+++ src/data/homepage-explore.js
@@ -0,0 +1,117 @@
+export const HomePageExplore = [
+    {
+        tag: 'Free',
+        courses: [
+            {
+                heading: "Learn HTML",
+                description: "This course covers the basic concepts of HTML including creating and structuring webpages",
+                level: "Beginner",
+                lessonNumber: 6
+            },
+            {
+                heading: "Learn CSS",
+                description: "This course covers the basics of CSS including styling, layout, and design principles",
+                level: "Beginner",
+                lessonNumber: 6
+            },
+            {
+                heading: "Responsive Web Design",
+                description: "This course covers the principles and techniques of creating responsive websites that work on all devices",
+                level: "Beginner",
+                lessonNumber: 6
+            }
+        ]  
+    },
+    {
+        tag: 'New to Coding',
+        courses: [
+            {
+                heading: "HTML",
+                description: "This course covers the basic concepts of HTML including creating and structuring webpages",
+                level: "Beginner",
+                lessonNumber: 6
+            },
+            {
+                heading: "CSS",
+                description: "This course covers the basics of CSS including styling, layout, and design principles",
+                level: "Beginner",
+                lessonNumber: 6
+            },
+            {
+                heading: "JavaScript",
+                description: "This course covers the principles and techniques of creating responsive websites that work on all devices",
+                level: "Beginner",
+                lessonNumber: 6
+            }
+        ]
+    },
+    {
+        tag: 'Most Popular',
+        courses: [
+            {
+                heading: "Java",
+                description: "This course covers the basics of Java programming including syntax, object-oriented concepts, and more",
+                level: "Beginner",
+                lessonNumber: 6
+            },
+            {
+                heading: "Python",
+                description: "This course covers the basics of Python programming including syntax, data structures, and more",
+                level: "Beginner",
+                lessonNumber: 6
+            },
+            {
+                heading: "C++",
+                description: "This course covers the basics of C++ programming including syntax, object-oriented concepts, and more",
+                level: "Beginner",
+                lessonNumber: 6
+            }
+        ]
+    },
+    {
+        tag: 'Skills Paths',
+        courses: [
+            {
+                heading: "Flask",
+                description: "This course covers the basics of Java programming including syntax, object-oriented concepts, and more",
+                level: "Beginner",
+                lessonNumber: 6
+            },
+            {
+                heading: "Django",
+                description: "This course covers the basics of Python programming including syntax, data structures, and more",
+                level: "Beginner",
+                lessonNumber: 6
+            },
+            {
+                heading: "Fast API",
+                description: "This course covers the basics of C++ programming including syntax, object-oriented concepts, and more",
+                level: "Beginner",
+                lessonNumber: 6
+            }
+        ]
+    },
+    {
+        tag: 'Career Paths',
+        courses: [
+            {
+                heading: "Next.js",
+                description: "This course covers the basics of Java programming including syntax, object-oriented concepts, and more",
+                level: "Beginner",
+                lessonNumber: 6
+            },
+            {
+                heading: "Sanity",
+                description: "This course covers the basics of Python programming including syntax, data structures, and more",
+                level: "Beginner",
+                lessonNumber: 6
+            },
+            {
+                heading: "Web3",
+                description: "This course covers the basics of C++ programming including syntax, object-oriented concepts, and more",
+                level: "Beginner",
+                lessonNumber: 6
+            }
+        ]
+    },
+];
